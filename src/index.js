8e87e557ed28523b8c78270e6cdea337d6fa644d
Julie Dickson <juliedickson227@gmail.com>
2024-03-20 13:07:00 -0400

    Added chooseHole function

--- src/index.js
+++ src/index.js
@@ -66,8 +66,14 @@ function setDelay(difficulty) {
  */
 function chooseHole(holes) {
   // TODO: Write your code here.
-
-}
+    const index = randomInteger(0, 8);
+    const hole = holes[index];
+    if (hole === lastHole) {
+      return chooseHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+  }
 
 /**
 *
